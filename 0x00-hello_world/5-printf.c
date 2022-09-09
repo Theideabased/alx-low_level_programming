a8510f06ec93df3f53133faa0398efb17437d82a
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-08 20:45:21 -0700

    c is fun

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but
-        the outcomes is a piece of art, ");
+        the outcomes is a piece of art, \n");
         return (0);
 }
