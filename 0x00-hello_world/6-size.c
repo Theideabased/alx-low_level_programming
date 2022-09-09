fcb66fcae6aaa8301577ddec2f0a63bf3630ba77
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-09 04:35:18 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 
 /**
- * this script will print the size of various
- * type on the computer and run it with compiler
+ * main - print the string in the put function
+ *
+ * Description : using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-/** Declaration of variable **/
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-/** Print all variables **/
-printf("Size of char: %lu byte(s)\n", sizeof(d));
-printf("Size of an int: %lu byte(s)\n", sizeof(a));
-printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
-return(0);
+
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+	
+	printf("Size of char: %ld byte(s)\n", sizeof(d));
+	printf("Size of an int: %ld byte(s)\n", sizeof(a));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return(0);
 }
 
