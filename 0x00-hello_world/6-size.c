d0c4b5be8e3c2b962709069655d898f5bcbf86ea
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-09 03:44:07 -0700

    c is challenging

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        /** Declaration of variable **/
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-	/** Print all variables **/
-	printf("Size of char: %lu byte(s)\n", sizeof(d));
-        printf("Size of an int: %lu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	return(0);
+/** Declaration of variable **/
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+/** Print all variables **/
+printf("Size of char: %lu byte(s)\n", sizeof(d));
+printf("Size of an int: %lu byte(s)\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return(0);
 }
 
