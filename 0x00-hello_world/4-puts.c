c5709eacb67b50c753047abb830a0f90e9f0a31e
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-08 19:51:55 -0700

    c is cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-int main()
+
+/**
+ * main -Entry point
+ * Description: prints the string "programming is like 
+ * building a multilingual puzzle"*
+ * And return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
