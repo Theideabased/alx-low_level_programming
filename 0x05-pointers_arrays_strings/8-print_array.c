f559826e7c26a9db228a550c4e62177ce79b8df9
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-20 10:27:59 -0700

    c is fun

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@
  */
 void print_array(int *a, int n)
 {
-	int inc;
-	for (inc = 0; inc < n; inc ++)
+	int inc ;
+	for (inc = 0 ; inc < n ; inc ++)
 	{
 		if (inc != n - 1)
-			printf("%d, ", a[inc]);
+			printf("%d, ", a [inc]);
 		else 
-			printf("%d", a[inc]);
+			printf("%d", a [inc]);
 	}
-	putchar (0);
+	putchar (10);
 }
