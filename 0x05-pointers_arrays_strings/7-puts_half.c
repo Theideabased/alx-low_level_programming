9775a380068336adf88f7a3d898cf976f75c764f
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-19 21:11:34 -0700

    c is fun

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half : ti print half of a string
+ * @str: tge steing to be print
+ *
+ * Return : nothing
+ */
+
+void puts_half(char *str)
+{
+	while (*(
