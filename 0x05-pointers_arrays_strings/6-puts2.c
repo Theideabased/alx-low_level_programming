ff58acafd533c377352306b09ef320e4c16ebe55
Theideabased <ogunmusireseyi01@gmail.com>
2022-09-19 19:52:13 -0700

    c is fun

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 : print other character in the line
+ * @str - the string to print from
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + 1) != '\0')
+	{
+		if (i % 2 != 0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar(10)
+}
