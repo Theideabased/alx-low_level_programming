707ad40cc47f1c6553e0f3cb48bd5fe3d2f931d3
Theideabased <ogunmusireseyi01@gmail.com>
2023-10-10 18:33:48 -0700

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+/**
+ * This is the header file 
+ * it contains all the prototype
+ */
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_H */
